3d2a8276c82a7bc9be4e964d60b016ebf7d6a2e8
kaizhelam <lamkaizhe2001@gmail.com>
2021-12-03 13:37:04 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const text = document.querySelector('.text');
+const bg = document.querySelector('.background');
+
+let load = 0;
+let int = setInterval(blurring, 30);
+
+function blurring(){
+    load++;
+    if(load > 99){
+        clearInterval(int);
+    }
+    text.innerHTML = load + '%';
+    text.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
